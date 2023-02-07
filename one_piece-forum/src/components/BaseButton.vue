c9384037ea2437b51310829b036f5fa76830fc55
marcossnikel <marcosnikeldev@gmail.com>
2023-02-07 05:54:42 -0300

    feature: button component

---
+++ one_piece-forum/src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: {
+    left: Boolean,
+    default: false,
+  },
+};
+</script>
+
+<template>
+  <button>
+    <slot v-if="left"> Submit </slot>
+    <slot v-else>Cancel</slot>
+  </button>
+</template>
