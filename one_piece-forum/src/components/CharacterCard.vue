a814d333b2e0648219da107bfde0a9372afce204
marcossnikel <marcosnikeldev@gmail.com>
2023-02-07 05:54:56 -0300

    feature: character card component

---
+++ one_piece-forum/src/components/CharacterCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["favorite"],
+  methods: {
+    favoriteCharacter() {
+      this.$emit("favorite", this.character);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>
+      Name : {{ character.name }} <br />
+      Age : {{ character.age }}
+    </p>
+    <button @click="favoriteCharacter">Favorite Char</button>
+  </div>
+</template>
