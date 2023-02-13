72a89b138d4d175d2929703fdcc0e5e25a0b1f69
marcossnikel <marcosnikeldev@gmail.com>
2023-02-13 19:34:06 -0300

    feat: basic feats vue

---
+++ one_piece-forum/src/components/scratch-pad.vue
@@ -0,0 +1,103 @@
+<script>
+import BaseButton from "./components/BaseButton.vue";
+import Counter from "./components/BaseCounter.vue";
+import LearningListCounter from "./components/LearningListCounter.vue";
+import OnePieceComponent from "./components/OnePieceComponent.vue";
+import StatisticsModule from "./components/StatisticsModule.vue";
+import UserCard from "./components/UserCard.vue";
+import BaseLayout from "./components/BaseLayout.vue";
+
+export default {
+  components: {
+    Counter,
+    OnePieceComponent,
+    LearningListCounter,
+    StatisticsModule,
+    UserCard,
+    BaseButton,
+    BaseLayout,
+  },
+
+  data: () => ({
+    persons: {
+      chars: [
+        {
+          name: "Luffy",
+          age: 18,
+          powers: ["Gum", "Haki", "Haoshoku", "Lightning"],
+        },
+        {
+          name: "Zoro",
+          age: 22,
+          powers: ["Sword", "Haki", "Strengh", "Haoshoku"],
+        },
+        {
+          name: "Nami",
+          age: 22,
+          powers: ["Lightning", "Zeus"],
+        },
+        {
+          name: "Sanji",
+          age: 28,
+          powers: ["Kick", "Haki", "DNA"],
+        },
+        {
+          name: "Robin",
+          age: 31,
+          powers: ["Knowledge", "Akuma no Mi", "Vision"],
+        },
+        {
+          name: "Usopp",
+          age: 23,
+          powers: ["Vision", "Toxic Plants"],
+        },
+        {
+          name: "Katakuri",
+          age: 54,
+          powers: [
+            "Vision Haki",
+            "Akuma no Mi",
+            "Knowledge",
+            "Vision",
+            "Haoshoku",
+          ],
+        },
+      ],
+    },
+    userData: {
+      name: "Nickel",
+      preferredFramework: "vue",
+      favoriteFood: "meat",
+      favoriteNumber: [7, 14, 21],
+    },
+  }),
+  computed: {},
+  methods: {
+    changeName() {
+      this.userData.name = "Livia";
+      this.userData.favoriteFood = "Macarrão";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <base-layout>
+      <template #sidebar> Aside </template>
+      <template #main>
+        <user-card
+          :name="userData.name"
+          :food="userData.favoriteFood"
+          @change-name="changeName"
+        />
+      </template>
+      <template #footer> <counter /> </template>
+    </base-layout>
+    <base-button> {{ userData.name }}</base-button>
+
+    <one-piece-component :charsProp="persons.chars" />
+    <learning-list-counter />
+    <statistics-module :characters="persons.chars" />
+  </div>
+</template>
