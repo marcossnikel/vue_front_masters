788b20aabc83bdc5e9194559193b54fcf08bb04d
marcossnikel <marcosnikeldev@gmail.com>
2023-02-13 22:34:04 -0300

    feat: user card

---
+++ cest-la-vue/src/components/UserCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li class="user-card">{{ user.name }} : {{ user.website }}</li>
+</template>
+
+<style>
+.user-card {
+  border: 2px solid black;
+  padding: 1rem;
+  list-style-type: none;
+}
+</style>
